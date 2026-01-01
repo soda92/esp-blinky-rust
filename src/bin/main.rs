de44466e83cddc5ed34931da6b013a147673038a
Jackson Wong <wjxa20152015@gmail.com>
2026-01-01 08:05:08 +0800

    split setup and main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,118 +1,36 @@
 #![no_std]
 #![no_main]
-#![deny(
-    clippy::mem_forget,
-    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
-    holding buffers for the duration of a data transfer."
-)]
-#![deny(clippy::large_stack_frames)]
 
-use esp_hal::clock::CpuClock;
-use esp_hal::timer::timg::TimerGroup;
-// 1. Add GPIO imports
-use bt_hci::controller::ExternalController;
-use esp_hal::gpio::{Level, Output, OutputConfig}; // <--- ADD THIS
-use esp_radio::ble::controller::BleConnector;
-use trouble_host::prelude::*;
-
+use esp_blinky_rust::{setup, ScanConfig, Duration, Timer};
 use rtt_target::rprintln;
-
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
 
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
     rprintln!("PANIC: {:?}", info);
     loop {}
 }
-use esp_radio::wifi::ScanConfig;
-
-extern crate alloc;
-
-const CONNECTIONS_MAX: usize = 1;
-const L2CAP_CHANNELS_MAX: usize = 1;
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
-// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
 
-#[allow(
-    clippy::large_stack_frames,
-    reason = "it's not unusual to allocate larger buffers etc. in main"
-)]
 #[esp_rtos::main]
 async fn main(spawner: Spawner) -> ! {
-    // generator version: 1.1.0
-
     rtt_target::rtt_init_print!();
-
-    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let peripherals = esp_hal::init(config);
-
-    esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 66320);
-    // COEX needs more RAM - so we've added some more
-    esp_alloc::heap_allocator!(size: 64 * 1024);
-
-    let timg0 = TimerGroup::new(peripherals.TIMG0);
-    let sw_interrupt =
-        esp_hal::interrupt::software::SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
-    esp_rtos::start(timg0.timer0, sw_interrupt.software_interrupt0);
-
-    rprintln!("Embassy initialized!");
-    // 2. Setup GPIO 8 (Blue LED)
-    // We do this before the radio stuff just to be safe, though order here usually doesn't matter
-    let mut led = Output::new(peripherals.GPIO8, Level::High, OutputConfig::default());
-
-    let radio_init = esp_radio::init().expect("Failed to initialize Wi-Fi/BLE controller");
-    let (mut wifi_controller, interfaces) = esp_radio::wifi::new(
-        &radio_init,
-        peripherals.WIFI,
-        esp_radio::wifi::Config::default(),
-    )
-    .expect("Failed to initialize Wi-Fi controller");
-
-    // We need to take the station interface to ensure the controller knows we are in STA mode
-    let _sta = interfaces.sta;
-
-    // Set the mode to Station (Client)
-    wifi_controller
-        .set_config(&esp_radio::wifi::ModeConfig::Client(
-            esp_radio::wifi::ClientConfig::default(),
-        ))
-        .unwrap();
-    // find more examples https://github.com/embassy-rs/trouble/tree/main/examples/esp32
-    let transport = BleConnector::new(&radio_init, peripherals.BT, Default::default()).unwrap();
-    let ble_controller = ExternalController::<_, 1>::new(transport);
-    let mut resources: HostResources<DefaultPacketPool, CONNECTIONS_MAX, L2CAP_CHANNELS_MAX> =
-        HostResources::new();
-    let _stack = trouble_host::new(ble_controller, &mut resources);
-
-    // TODO: Spawn some tasks
-    let _ = spawner;
-
-    // CHANGE 2: Start the WiFi controller!
-    // You must turn it on before you can scan.
-    rprintln!("Starting WiFi...");
-    wifi_controller.start_async().await.unwrap();
+    // Setup the application (heap, peripherals, radio, etc.)
+    let mut app = setup(spawner).await;
 
     loop {
         rprintln!("Scanning...");
-        led.set_low(); // Turn LED ON while scanning
+        app.led.set_low(); // Turn LED ON while scanning
 
-        // CHANGE 3: The Scan Command
-        // scan_n::<10> means "find up to 10 networks"
-        let m = wifi_controller
-            .scan_with_config_async(ScanConfig::default())
-            .await
-            .unwrap();
+        let m = app.wifi.scan_with_config_async(ScanConfig::default()).await.unwrap();
 
         for ap in m {
             rprintln!("Found: {:?} | RSSI: {}", ap.ssid, ap.signal_strength);
         }
 
-        led.set_high(); // Turn LED OFF
+        app.led.set_high(); // Turn LED OFF
         Timer::after(Duration::from_secs(5)).await;
     }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v~1.0/examples
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,90 @@
 #![no_std]
+#![deny(clippy::large_stack_frames)]
+
+use esp_hal::clock::CpuClock;
+use esp_hal::timer::timg::TimerGroup;
+use esp_hal::gpio::{Level, Output, OutputConfig};
+use esp_radio::ble::controller::BleConnector;
+use bt_hci::controller::ExternalController;
+use trouble_host::prelude::*;
+use rtt_target::rprintln;
+use esp_radio::wifi::{WifiController, Config, ModeConfig, ClientConfig};
+use alloc::boxed::Box;
+
+extern crate alloc;
+
+// Re-exports for main.rs
+pub use esp_radio::wifi::ScanConfig;
+pub use embassy_time::{Duration, Timer};
+pub use embassy_executor::Spawner;
+pub use rtt_target::rtt_init_print;
+
+const CONNECTIONS_MAX: usize = 1;
+const L2CAP_CHANNELS_MAX: usize = 1;
+
+// Define the type for the BLE Stack to simplify return types
+pub type BleStack<'a> = Stack<'a, ExternalController<BleConnector<'a>, 1>, DefaultPacketPool>;
+
+pub struct AppState {
+    pub led: Output<'static>,
+    pub wifi: WifiController<'static>,
+    pub ble_stack: BleStack<'static>,
+}
+
+pub async fn setup(_spawner: Spawner) -> AppState {
+    // 1. Initialize Heap
+    esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 66320);
+    esp_alloc::heap_allocator!(size: 64 * 1024);
+
+    // 2. Initialize Peripherals
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    // 3. Initialize RTOS & Interrupts
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    let sw_interrupt =
+        esp_hal::interrupt::software::SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
+    esp_rtos::start(timg0.timer0, sw_interrupt.software_interrupt0);
+
+    rprintln!("Embassy initialized!");
+
+    // 4. Initialize LED
+    let led = Output::new(peripherals.GPIO8, Level::High, OutputConfig::default());
+
+    // 5. Initialize Radio (WiFi & BLE)
+    // We leak the radio_init to get a 'static reference, allowing us to return controllers
+    // that reference it.
+    let radio_init = esp_radio::init().expect("Failed to initialize Wi-Fi/BLE controller");
+    let radio_init: &'static _ = Box::leak(Box::new(radio_init));
+    
+    // WiFi Setup
+    let (mut wifi_controller, interfaces) =
+        esp_radio::wifi::new(radio_init, peripherals.WIFI, Config::default())
+            .expect("Failed to initialize Wi-Fi controller");
+    
+    // Ensure STA mode
+    let _sta = interfaces.sta;
+    wifi_controller.set_config(&ModeConfig::Client(ClientConfig::default())).unwrap();
+    
+    // Start WiFi
+    rprintln!("Starting WiFi...");
+    wifi_controller.start_async().await.unwrap();
+
+    // BLE Setup
+    let transport = BleConnector::new(radio_init, peripherals.BT, Default::default()).unwrap();
+    let ble_controller = ExternalController::<_, 1>::new(transport);
+    
+    // Use StaticCell for resources to ensure they live forever
+    static RESOURCES: static_cell::StaticCell<HostResources<DefaultPacketPool, CONNECTIONS_MAX, L2CAP_CHANNELS_MAX>> = static_cell::StaticCell::new();
+    let resources = RESOURCES.init(HostResources::new());
+    
+    let ble_stack = trouble_host::new(ble_controller, resources);
+
+    rprintln!("Setup complete.");
+
+    AppState {
+        led,
+        wifi: wifi_controller,
+        ble_stack,
+    }
+}
